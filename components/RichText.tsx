945cbe817fa2c39dfb8efe42994f424846747bde
David Barcenas <daveepro@outlook.com>
2021-08-14 16:08:14 -0500

    design slug page and rich text component

---
+++ components/RichText.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BLOCKS, Document } from '@contentful/rich-text-types'
+import { documentToReactComponents, Options, } from '@contentful/rich-text-react-renderer'
+
+interface RichTextProps {
+    description: RichText;
+}
+
+const options: Options = {
+    renderNode: {
+        [BLOCKS.PARAGRAPH]: (_, children) => (
+            <p>{children}</p>
+        )
+    }
+}
+
+export const RichText = ({description}: RichTextProps) => {
+    const document: Document = description
+
+    return (
+        <>
+            {documentToReactComponents(document, options)}
+        </>
+    )
+}
--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -1,17 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router';
 import Error from 'next/error'
+import { useRouter } from 'next/router';
 import { getPlant, QueryStatus } from '../../api/index';
-import { BLOCKS, Document } from '@contentful/rich-text-types'
-import { documentToReactComponents, Options, } from '@contentful/rich-text-react-renderer'
-
-const options: Options = {
-    renderNode: {
-        [BLOCKS.PARAGRAPH]: (_, children) => (
-            <p>{children}</p>
-        )
-    }
-}
+import { RichText } from '@components/RichText';
+import { Layout } from '../../components/Layout';
 
 
 const PlantEntryPage = () => {
@@ -35,9 +27,6 @@ const PlantEntryPage = () => {
             .catch(() => setStatus('error'))
     }, [slug])
 
-    
-    
-
     if(status === 'loading' || status === 'idle') {
        return (
         <div>
@@ -50,43 +39,92 @@ const PlantEntryPage = () => {
         return <Error statusCode={404} />
     }
 
-    const document: Document = plant.description
-
     return (
-        <div>
-            <img src={plant.image.url} alt={plant.image.title} />
-            <h1>{plant.plantName}</h1>
-            {documentToReactComponents(document, options)}
-            <section>
-                <h3>Recent Post</h3>
-            </section>
-            <section>
-                <h3>Categories</h3>
-            </section>
-            <section>
-                <img src={plant.author.photo.url} alt={plant.author.photo.title} />
-                <h3>{plant.author.fullName}</h3>
-                <p>{plant.author.biography}</p>
-                <div className="flex">
-                <a
-                    href={plant.author.linkedIn}
-                    title={`Follow ${plant.author.fullName} on LinkedIn`}
-                    target="_blank"
-                    className="pr-4"
-                >
-                    LI
-                </a>
-                <a
-                    href={plant.author.twitter}
-                    title={`Follow ${plant.author.fullName} on Twitter`}
-                    target="_blank"
-                    className="pr-4"
-                >
-                    TW
-                </a>
+        <>
+            <Layout>
+                <div className="post-container">
+                    <article className="post">
+                        <img src={plant.image.url} alt={plant.image.title} />
+                        <h2 className="post-title">{plant.plantName}</h2>
+                        <RichText description={plant.description} />
+                        <footer className="post-footer">
+                            <img src={plant.author.photo.url} alt={plant.author.photo.title} />
+                            <div className="article-author">
+                                <h3>{plant.author.fullName}</h3>
+                                <p>{plant.author.biography}</p>
+                                <div className="flex">
+                                    <a
+                                        href={plant.author.linkedIn}
+                                        title={`Follow ${plant.author.fullName} on LinkedIn`}
+                                        target="_blank"
+                                    >
+                                        LI
+                                    </a>
+                                    <a
+                                        href={plant.author.twitter}
+                                        title={`Follow ${plant.author.fullName} on Twitter`}
+                                        target="_blank"
+                                    >
+                                        TW
+                                    </a>
+                                </div>
+                            </div>
+                        </footer>
+                    </article>
+                    <aside className="post-aside">
+                        <section>
+                            <h3>Recent Post</h3>
+                        </section>
+                        <section>
+                            <h3>Categories</h3>
+                        </section>
+                    </aside>
                 </div>
-            </section>
-        </div>
+            </Layout>
+
+            <style jsx>{`
+                .post-container {
+                    display: flex;
+                    flex-wrap: wrap;
+                }
+                .post-title {
+                    font-size: 1.5rem;
+                    margin: 1rem 0;
+                }
+                .post-aside h3 {
+                    margin: 0;
+                }
+                .post-footer {
+                    border: 1px solid #ccc;
+                    display: flex;
+                    align-items: center;
+                    padding: 1rem;
+                }
+
+                .post-footer img {
+                    width: 150px;
+                    height: 150px;
+                    border-radius: 50%;
+                    margin-right: 1.5rem;
+                }
+
+                .post-footer h3 {
+                    margin: 0;
+                }
+
+                @media screen and (min-width: 480px) {
+                    .post {
+                        width: 70%;
+                        padding-right: 1.5rem;
+                    }
+
+                    .post-aside {
+                        width: 30%;
+                        padding: 0 1rem;
+                    }
+                }
+            `}</style>
+        </>
     )
 }
 
