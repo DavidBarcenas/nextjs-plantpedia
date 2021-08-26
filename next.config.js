da7261534393d93b33b01ee42b24163e46e0bbf9
David Barcenas <daveepro@outlook.com>
2021-08-26 12:18:34 -0500

    conig bundle-analyzer, webpack5 and i18n

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-    images: {
-      domains: ['images.ctfassets.net'],
-    },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const config = {
+  webpack5: true,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+  i18n: {
+    locales: ['en-US', 'es-MX'],
+    defaultLocale: 'en-US',
   }
+}
+
+module.exports = withBundleAnalyzer(config)
